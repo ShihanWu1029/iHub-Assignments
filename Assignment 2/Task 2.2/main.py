5ff040b884fcf20b9f2f0c5398e122f3f47958be
ShihanWu1029 <iNever001029@outlook.com>
2025-07-16 09:38:41 +0800

    polynomial regression hw updated

--- Assignment 2/Task 2.2/main.py
+++ Assignment 2/Task 2.2/main.py
@@ -3,30 +3,31 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 
-# 生成一些示例数据
-# np.random.seed()
-X = np.sort(5 * np.random.rand(80, 1), axis=0)
-# y = np.sin(X).ravel() + np.random.randn(80) * 0.1
-y= X
+# Data generation
+# np.random.seed(0)
+X = np.sort(255 * np.random.rand(80, 1), axis=0)
+# given f(x) = -(x-127)^2 + 5 + noise
+y= -(X.ravel()-127)**2+5 + np.random.randn(80) * 1000
 
-# 创建多项式特征
+
 poly_features = PolynomialFeatures(degree=3)
 X_poly = poly_features.fit_transform(X)
 
-# 拟合线性回归模型
 lin_reg = LinearRegression()
 lin_reg.fit(X_poly, y)
 
-# 预测
-X_test = np.linspace(0, 5, 100).reshape(-1, 1)
+X_test = np.linspace(0, 255, 1000).reshape(-1, 1)
 X_test_poly = poly_features.transform(X_test)
 y_pred = lin_reg.predict(X_test_poly)
+
+print("Prediction at x =",int(X_test[999][0]),end=' ')
+print("is y =",y_pred[999])
 
 # 绘制结果
 plt.scatter(X, y, color='blue', label='Data')
 plt.plot(X_test, y_pred, color='red', label='Polynomial Regression')
-plt.xlabel('X')
-plt.ylabel('y')
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
 plt.title('Polynomial Regression')
 plt.legend()
 plt.show()
