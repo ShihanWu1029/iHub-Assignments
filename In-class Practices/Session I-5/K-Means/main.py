0c82322e421a7a7670ac9b6f08e1c0457f64fd76
ShihanWu1029 <iNever001029@outlook.com>
2025-07-23 16:10:31 +0800

    upload session I-5 of kmeans practise

---
+++ In-class Practices/Session I-5/K-Means/main.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+
+X, y_true = make_blobs(n_samples=555, centers=5, cluster_std=0.5, random_state=0)
+
+k_means = KMeans(n_clusters=5, random_state=42)
+k_means.fit(X)
+y_kmeans = k_means.predict(X)
+
+plt.figure(figsize=(10, 6))
+plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
+
+centers = k_means.cluster_centers_
+
+plt.scatter(centers[:, 0], centers[:, 1], c='red', s=200, alpha=0.75, marker='x', label='centers')
+plt.title('K-means Result')
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.legend()
+plt.grid(True, linestyle='--', alpha=0.7)
+plt.show()
